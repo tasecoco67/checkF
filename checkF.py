b381cd69a245a41fc257639945ca3cff40f05c93
tasecoco67 <>
2020-11-25 23:01:46 +0900

    first commit

---
+++ checkF.py
@@ -0,0 +1,74 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import time
+
+driver = webdriver.Chrome()
+
+def twfcheck():
+    driver.get(setting.TWR)
+    time.sleep(2)
+    twf = driver.find_element_by_xpath(r'//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div/div[1]/div/div[5]/div[2]/a/span[1]/span').text
+    return twf;
+#twfcheck();
+
+def infcheck():
+    driver.get(setting.IUR)
+    time.sleep(2);
+    inf=driver.find_element_by_xpath(r'//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a/span').text
+    return inf;
+#infcheck()
+
+def ntfcheck():
+    driver.get(setting.NUR)
+    time.sleep(2)
+    ntf=driver.find_element_by_xpath(r'//*[@id="__layout"]/div/main/header/div/div[2]/div/div[2]/div[2]/div/div/div[1]/a[2]/span[1]').text
+    return ntf;
+
+def ntvcheck():
+    driver.get(setting.NUR2)
+    driver.find_element_by_name('login').send_keys(setting.MAIL)
+    driver.find_element_by_name('password').send_keys(setting.PSW)
+    #ログインボタンをクリック
+    driver.find_element_by_xpath(r'/html/body/main/login/div/section/div/div/form/button').click()
+    time.sleep(3);
+    driver.get(setting.NUR3)
+    time.sleep(2);
+    driver.find_element_by_xpath(r'//*[@id="__layout"]/div/div[2]/div[2]/div/div/div/div[1]/div[1]/ul/li[3]/button').click()
+    time.sleep(2)
+    nv = driver.find_element_by_xpath(r'//*[@id="__layout"]/div/div[2]/div[2]/div/div/div/div[2]/div[1]/div[2]').text
+    ng = driver.find_element_by_xpath(r'//*[@id="__layout"]/div/div[2]/div[2]/div/div/div/div[2]/div[3]/div[2]').text
+    return nv,ng
+
+#print(ntvcheck()[0])
+
+def writegs():
+    import gspread
+    import json
+    #ServiceAccountCredentials：Googleの各サービスへアクセスできるservice変数を生成します。
+    from oauth2client.service_account import ServiceAccountCredentials
+
+    #2つのAPIを記述しないとリフレッシュトークンを3600秒毎に発行し続けなければならない
+    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+
+    credential = {
+      setting.CREDEANTIAL
+    }
+
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(credential, scope)
+
+    gc = gspread.authorize(credentials)
+
+    #共有設定したスプレッドシートのシート1を開く
+    workbook = gc.open_by_key(setting.SPK)
+    worksheet1 = workbook.worksheet('insta')
+    worksheet2 = workbook.worksheet('note')
+    worksheet3 = workbook.worksheet('twitter')
+    worksheet1.update_acell("I2", infcheck())
+    worksheet2.update_acell("G2", ntfcheck())
+    m = ntvcheck()
+    worksheet2.update_acell("F5", m[0])
+    worksheet2.update_acell("G5", m[1])
+    worksheet3.update_acell("G2", twfcheck())
+writegs();
---
+++ setting.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+# .envファイルの内容を読み込みます
+load_dotenv()
+
+IUR = os.environ.get("INURL")
+TWR = os.environ.get("TWURL")
+NUR = os.environ.get("NTURL")
+NUR2 = os.environ.get("NTURL2")
+MAIL = os.environ.get("MAIL")
+PSW = os.environ.get("PSW")
+NUR3 = os.environ.get("NTURL3")
+SPK = os.environ.get("SPREADSHEET_KEY")
+CREDEANTIAL = os.environ.get("CREDEANTIAL")
